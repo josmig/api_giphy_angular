a4c9c5e3659980d4c6ac9bb121496e2db0176ae7
josmig <pantigosomiguel59@gmail.com>
2021-09-08 11:57:18 -0500

    guardar historial en el localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { SearchGifsResponse, Gif } from './../interfaces/gifs.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -9,15 +10,21 @@ export class GifsService {
   private api_key: string = 'n0MCAVNSMzWUnGH8lQzT1T0t2IpmDxnr';
   private _historial: string[] = [];
   
-  //TODO: Cambiar any por su tipo
-  public resultados:any [] = [];
+  //TODO: Cambiar any por su tipo Interface
+  public resultados: Gif[] = [];
 
   get historial(){    
     return [...this._historial];
     
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    //cargar el localstorage | Historial
+    this._historial = JSON.parse(localStorage.getItem('historial')! )||[];
+    /* if( localStorage.getItem('historial')){
+      this._historial = JSON.parse( localStorage.getItem('historial')!);
+    } */
+  }
 
 
   buscarGifs( query: string){
@@ -29,17 +36,23 @@ export class GifsService {
       this._historial.unshift( query );
       //limitando la cantidad de info en el historial
       this._historial =this._historial.splice(0,10);
+
+      //Localstorage | Utilizamos el JSOn para pasar a string cualquier tipo de dato
+      localStorage.setItem('historial',JSON.stringify(this._historial));
+      //con esto se guarda en el localstorage
     }
     
       //Forma de consumir en vez del fechAPI poque cuando ocurre un error tendriamos que hacer un monton de cosas mas
-     this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=n0MCAVNSMzWUnGH8lQzT1T0t2IpmDxnr&q=${query}&limit=10`)
-      .subscribe( (respuesta:any) => {       
+     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=n0MCAVNSMzWUnGH8lQzT1T0t2IpmDxnr&q=${query}&limit=10`)
+      .subscribe( (respuesta) => {       
         console.log( respuesta.data );
         this.resultados = respuesta.data;
       });
 
   }
 
+
+  
   
 
 }
